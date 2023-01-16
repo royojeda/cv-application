11eaefff3725c79b7006b813a9744108d72f337a
Dennis Ojeda <royojeda1@gmail.com>
2023-01-16 14:47:14 +0800

    Initial commit
    Using Create React App, TypeScript, and Tailwind CSS.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <div>Hello, world!</div>
+      </div>
+    );
+  }
+}
