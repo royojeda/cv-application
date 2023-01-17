f740d0027c2e22520df341cde1e7f57903816fa3
Dennis Ojeda <royojeda1@gmail.com>
2023-01-17 19:28:54 +0800

    Add an xs breakpoint
    Min width 550px.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "550px",
+      ...defaultTheme.screens,
+    },
     extend: {},
   },
   plugins: [],
