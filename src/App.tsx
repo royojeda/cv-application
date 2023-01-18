00957ef0140f7a7f085cc0d2246fc206b56c20c8
Dennis Ojeda <royojeda1@gmail.com>
2023-01-18 21:29:42 +0800

    Update GeneralInformation's edit button rendering
    Show the edit button only when user hovers over the component.
    
    Updated styles.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     return (
       <div className="flex min-h-screen w-screen text-gray-700">
         <div className="flex flex-1 items-center justify-center p-4 sm:p-10">
-          <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-6 border border-gray-700 p-4 shadow-md sm:p-10">
+          <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-6 border p-4 shadow-md sm:p-10">
             <GeneralInformation />
             <div>Experience</div>
             <div>Education</div>
--- src/components/GeneralInformation.tsx
+++ src/components/GeneralInformation.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import UserInfo from "./UserInfo";
 
 export default class GeneralInformation extends React.Component<
   Record<string, never>,
-  { name: string; emailAddress: string; phoneNumber: string; address: string }
+  {
+    name: string;
+    emailAddress: string;
+    phoneNumber: string;
+    address: string;
+    isEditing: boolean;
+  }
 > {
   constructor(props: Record<string, never>) {
     super(props);
@@ -12,14 +18,37 @@ export default class GeneralInformation extends React.Component<
       emailAddress: "johndoe@email.com",
       phoneNumber: "123 456 7890",
       address: "221b Baker Street, London",
+      isEditing: false,
     };
   }
 
+  handleMouseEnter = (event: SyntheticEvent) => {
+    event.currentTarget.classList.replace("border-white", "border-gray-200");
+    event.currentTarget.classList.add("shadow-md");
+    event.currentTarget.classList.remove("transition", "duration-200");
+    this.setState({ isEditing: true });
+  };
+
+  handleMouseLeave = (event: SyntheticEvent) => {
+    event.currentTarget.classList.replace("border-gray-200", "border-white");
+    event.currentTarget.classList.remove("shadow-md");
+    event.currentTarget.classList.add("transition", "duration-200");
+    this.setState({ isEditing: false });
+  };
+
   render() {
-    const { name, emailAddress, phoneNumber, address } = this.state;
+    const { name, emailAddress, phoneNumber, address, isEditing } = this.state;
     return (
-      <div className="general-information relative flex flex-col items-center gap-4">
-        <button className="absolute right-0">Edit</button>
+      <div
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        className="general-information relative flex flex-col items-center gap-4 border border-white p-4"
+      >
+        {isEditing && (
+          <button className="absolute right-4 top-4 border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800">
+            Edit
+          </button>
+        )}
         <div className="text-center text-4xl font-medium">{name}</div>
         <div className="flex w-full flex-col items-center justify-center gap-4 xs:flex-row">
           <UserInfo contentType="email" content={emailAddress} />
