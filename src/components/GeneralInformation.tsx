0f5f76f292ada822c74895bfdd10a02a637be1ef
Dennis Ojeda <royojeda1@gmail.com>
2023-01-19 23:58:17 +0800

    Extract a GeneralInformationDisplay component

--- src/components/GeneralInformation.tsx
+++ src/components/GeneralInformation.tsx
@@ -1,12 +1,12 @@
-import React, { SyntheticEvent } from "react";
-import UserInfo from "./UserInfo";
+import React from "react";
+import GeneralInformationDisplay from "./GeneralInformationDisplay";
 
 interface GeneralInformationState {
   name: string;
   emailAddress: string;
   phoneNumber: string;
   address: string;
-  isHovered: boolean;
+  isEditing: boolean;
 }
 
 export default class GeneralInformation extends React.Component<
@@ -20,44 +20,28 @@ export default class GeneralInformation extends React.Component<
       emailAddress: "johndoe@email.com",
       phoneNumber: "123 456 7890",
       address: "221b Baker Street, London",
-      isHovered: false,
+      isEditing: false,
     };
   }
 
-  handleMouseEnter = (event: SyntheticEvent) => {
-    event.currentTarget.classList.replace("border-white", "border-gray-200");
-    event.currentTarget.classList.add("shadow-md");
-    event.currentTarget.classList.remove("transition", "duration-200");
-    this.setState({ isHovered: true });
-  };
-
-  handleMouseLeave = (event: SyntheticEvent) => {
-    event.currentTarget.classList.replace("border-gray-200", "border-white");
-    event.currentTarget.classList.remove("shadow-md");
-    event.currentTarget.classList.add("transition", "duration-200");
-    this.setState({ isHovered: false });
+  handleEdit = () => {
+    this.setState({ isEditing: true });
   };
 
   render() {
-    const { name, emailAddress, phoneNumber, address, isHovered } = this.state;
+    const { name, emailAddress, phoneNumber, address, isEditing } = this.state;
     return (
-      <div
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
-        className="general-information relative flex flex-col items-center gap-4 border border-white p-4"
-      >
-        {isHovered && (
-          <button className="absolute right-4 top-4 border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800">
-            Edit
-          </button>
+      <>
+        {isEditing ? null : (
+          <GeneralInformationDisplay
+            name={name}
+            emailAddress={emailAddress}
+            phoneNumber={phoneNumber}
+            address={address}
+            onEdit={this.handleEdit}
+          />
         )}
-        <div className="text-center text-4xl font-medium">{name}</div>
-        <div className="flex w-full flex-col items-center justify-center gap-4 xs:flex-row">
-          <UserInfo type="emailAddress" content={emailAddress} />
-          <UserInfo type="phoneNumber" content={phoneNumber} />
-          <UserInfo type="address" content={address} />
-        </div>
-      </div>
+      </>
     );
   }
 }
---
+++ src/components/GeneralInformationDisplay.tsx
@@ -0,0 +1,67 @@
+import React, { MouseEvent } from "react";
+import UserInfo from "./UserInfo";
+
+interface GeneralInformationDisplayProps {
+  name: string;
+  emailAddress: string;
+  phoneNumber: string;
+  address: string;
+  onEdit: () => void;
+}
+
+interface GeneralInformationDisplayState {
+  isHovered: boolean;
+}
+
+export default class GeneralInformationDisplay extends React.Component<
+  GeneralInformationDisplayProps,
+  GeneralInformationDisplayState
+> {
+  constructor(props: GeneralInformationDisplayProps) {
+    super(props);
+    this.state = {
+      isHovered: false,
+    };
+  }
+
+  handleMouseEnter = (event: MouseEvent) => {
+    event.currentTarget.classList.replace("border-white", "border-gray-200");
+    event.currentTarget.classList.add("shadow-md");
+    event.currentTarget.classList.remove("transition", "duration-200");
+    this.setState({ isHovered: true });
+  };
+
+  handleMouseLeave = (event: MouseEvent) => {
+    event.currentTarget.classList.replace("border-gray-200", "border-white");
+    event.currentTarget.classList.remove("shadow-md");
+    event.currentTarget.classList.add("transition", "duration-200");
+    this.setState({ isHovered: false });
+  };
+
+  render() {
+    const { name, emailAddress, phoneNumber, address, onEdit } = this.props;
+
+    return (
+      <div
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}
+        className="general-information relative flex flex-col items-center gap-4 border border-white p-4"
+      >
+        {this.state.isHovered && (
+          <button
+            onClick={onEdit}
+            className="absolute right-4 top-4 border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800"
+          >
+            Edit
+          </button>
+        )}
+        <div className="text-center text-4xl font-medium">{name}</div>
+        <div className="flex w-full flex-col items-center justify-center gap-4 xs:flex-row">
+          <UserInfo type="emailAddress" content={emailAddress} />
+          <UserInfo type="phoneNumber" content={phoneNumber} />
+          <UserInfo type="address" content={address} />
+        </div>
+      </div>
+    );
+  }
+}
