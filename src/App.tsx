6b77ef00b0929c102a0958cefea522305520d811
Dennis Ojeda <royojeda1@gmail.com>
2023-01-19 15:18:21 +0800

    Update the background color for more contrast

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="flex min-h-screen w-screen text-gray-700">
-        <div className="flex flex-1 items-center justify-center p-4 sm:p-10">
-          <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-6 border p-4 shadow-md sm:p-10">
+        <div className="flex flex-1 items-center justify-center bg-gray-50 p-4 sm:p-10">
+          <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-6 border bg-white p-4 shadow-md sm:p-10">
             <GeneralInformation />
             <div>Experience</div>
             <div>Education</div>
