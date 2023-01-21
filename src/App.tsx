cc9328bb4f55bd6d8194414623fa09a316b46721
Dennis Ojeda <royojeda1@gmail.com>
2023-01-22 03:07:36 +0800

    Extract an Experience component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Experience from "./components/Experience";
 import GeneralInformation from "./components/GeneralInformation";
 
 export default class App extends React.Component {
@@ -8,7 +9,7 @@ export default class App extends React.Component {
         <div className="flex flex-1 items-center justify-center bg-gray-50 p-4 sm:p-10">
           <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-6 border bg-white p-4 shadow-md sm:p-10">
             <GeneralInformation />
-            <div>Experience</div>
+            <Experience />
             <div>Education</div>
           </div>
         </div>
---
+++ src/components/Experience.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Experience extends React.Component {
+  render() {
+    return (
+      <div className="flex flex-col gap-1">
+        <div>Experience</div>
+      </div>
+    );
+  }
+}
