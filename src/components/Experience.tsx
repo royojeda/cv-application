190bb7e67867ded4743a5918966c40be21e0db2d
Dennis Ojeda <royojeda1@gmail.com>
2023-01-22 22:11:51 +0800

    Add an Experience entry template

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -7,6 +7,32 @@ export default class Experience extends React.Component {
         <div className="border-y border-gray-700 text-center text-lg font-medium">
           EXPERIENCE
         </div>
+        <div className="flex flex-col gap-1">
+          <div className="flex items-end justify-between gap-4">
+            <div className="flex">
+              <div className="font-medium">Some Big Company</div>
+              <pre>, </pre>
+              <div className="italic">Software Engineer</div>
+            </div>
+            <div className="flex">
+              <div>April 2022</div>
+              <pre> — </pre>
+              <div>January 2023</div>
+            </div>
+          </div>
+          <ul className="list-disc pl-8">
+            <li>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+              possimus aliquam amet, ipsum in placeat sint? Nobis obcaecati sint
+              aliquid.
+            </li>
+            <li>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+              distinctio assumenda, nemo quis necessitatibus tempora commodi
+              porro debitis cum itaque?
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
