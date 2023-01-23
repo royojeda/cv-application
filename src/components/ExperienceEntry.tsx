d4518a491a2ac3b3b1e81f64c9cd0b94ed264c15
Dennis Ojeda <royojeda1@gmail.com>
2023-01-23 14:38:19 +0800

    Extract an ExperienceEntry component

---
+++ src/components/ExperienceEntry.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default class ExperienceEntry extends React.Component {
+  render() {
+    return (
+      <div className="flex flex-col gap-1">
+        <div className="flex items-end justify-between gap-4">
+          <div className="flex">
+            <div className="font-medium">Some Big Company</div>
+            <pre>, </pre>
+            <div className="italic">Software Engineer</div>
+          </div>
+          <div className="flex">
+            <div>April 2022</div>
+            <pre> — </pre>
+            <div>January 2023</div>
+          </div>
+        </div>
+        <ul className="list-disc pl-8">
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti
+            possimus aliquam amet, ipsum in placeat sint? Nobis obcaecati sint
+            aliquid.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+            distinctio assumenda, nemo quis necessitatibus tempora commodi porro
+            debitis cum itaque?
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
