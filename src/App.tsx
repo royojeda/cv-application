7507fdc0aca09aa79379078e1eb3cc44cba58f1a
Dennis Ojeda <royojeda1@gmail.com>
2023-01-23 21:14:21 +0800

    Update App's styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ export default class App extends React.Component {
     return (
       <div className="flex min-h-screen w-screen text-gray-700">
         <div className="flex flex-1 items-center justify-center bg-gray-50 p-4 sm:p-10">
-          <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-6 border bg-white p-4 shadow-md sm:p-10">
+          <div className="flex min-h-full max-w-2xl flex-1 flex-col gap-1 border bg-white p-4 shadow-md sm:p-8">
             <GeneralInformation />
             <Experience />
-            <div className="border-y border-gray-700 text-center text-lg font-medium">
-              EDUCATION
+            <div className="-my-2.5 p-4">
+              <div className="border-y border-gray-700 text-center text-lg font-medium">
+                EDUCATION
+              </div>
             </div>
           </div>
         </div>
