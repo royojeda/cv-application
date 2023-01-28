314af8cd8c6f3277e81ce5bf96fec2c1bf7a96cd
Dennis Ojeda <royojeda1@gmail.com>
2023-01-29 04:09:27 +0800

    Update ExperienceEntryForm's styles
    Updated to match EducationEntryForm's styles.

--- src/components/ExperienceEntryForm.tsx
+++ src/components/ExperienceEntryForm.tsx
@@ -72,22 +72,24 @@ export default class ExperienceEntryForm extends React.Component<
     return (
       <form
         onSubmit={this.handleSubmit}
-        className="relative flex flex-col gap-1 border border-transparent px-4 py-2 hover:border-gray-200 hover:shadow-md"
+        className="relative flex flex-col gap-1 border border-transparent border-gray-200 px-4 py-2 shadow-md"
       >
-        <div className="absolute -right-32 -top-[1px] -bottom-[1px] flex flex-col justify-center gap-2 border bg-white p-4 shadow-md">
-          <button
-            type="submit"
-            className="border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800"
-          >
-            {this.props.entry ? "Update" : "Create"}
-          </button>
-          <button
-            type="button"
-            onClick={this.handleCancel}
-            className="border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800"
-          >
-            Cancel
-          </button>
+        <div className="absolute inset-y-[-1px] -right-40 flex items-center">
+          <div className="flex justify-center bg-white shadow-md">
+            <button
+              type="submit"
+              className="w-1/2 border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800"
+            >
+              {this.props.entry ? "Update" : "Create"}
+            </button>
+            <button
+              type="button"
+              onClick={this.handleCancel}
+              className="w-1/2 border py-1 px-3 shadow transition hover:bg-gray-700 hover:text-white active:bg-gray-800"
+            >
+              Cancel
+            </button>
+          </div>
         </div>
         <div className="flex items-end justify-between gap-4">
           <div className="flex">
